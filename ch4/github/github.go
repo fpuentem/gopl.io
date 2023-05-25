95f3805a0ef56e85875722b966e1afebcc031aeb
Fab <fpuentem@uni.pe>
2023-05-25 02:30:53 -0300

    Fix github package

--- ch4/github/github.go
+++ ch4/github/github.go
@@ -2,12 +2,19 @@
 // See https://developer.github.com/v3/search/#search-issues.
 package github
 
-import "time"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+)
 
 const IssuesURL = "https://api.github.com/search/issues"
 
 type IssuesSearchResult struct {
-	TotalCount int `json: "total_count"`
+	TotalCount int
 	Items      []*Issue
 }
 
@@ -24,4 +31,26 @@ type Issue struct {
 type User struct {
 	Login   string
 	HTMLURL string `json:"html_url"`
+}
+
+// SearchIssues queries the Github issue tracker
+func SearchIssues(terms []string) (*IssuesSearchResult, error) {
+	q := url.QueryEscape(strings.Join(terms, " "))
+	resp, err := http.Get(IssuesURL + "?q=" + q)
+	if err != nil {
+		return nil, err
+	}
+	// We must close resp.Body on all execution paths.
+	// (Chapter 5 presents 'defer', which make this simpler	)
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("search query failed: %s", resp.Status)
+	}
+	var result IssuesSearchResult
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		resp.Body.Close()
+		return nil, err
+	}
+	resp.Body.Close()
+	return &result, nil
 }
